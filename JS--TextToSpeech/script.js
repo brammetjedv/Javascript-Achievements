cba4e6efebe08ee57bb27cadacf10785642394ea
brammetjedv <bramdevries2003@gmail.com>
2021-01-21 15:42:26 +0100

    oompf

---
+++ JS--TextToSpeech/script.js
@@ -0,0 +1,62 @@
+    "use strict"   
+    let myText = document.getElementById('myText');
+    let myImage = document.getElementById('myImage');
+    let myButton = document.getElementById('myButton');
+    let myReset = document.getElementById('myReset');
+
+    myButton.addEventListener('click', function(){
+        main();
+    })
+
+    myReset.addEventListener('click', function(){
+        /* hier komt jouw code */
+    })
+
+    function main(){
+         let dezeTextWordtHet = makeThisSentence();
+         myText.innerHTML = dezeTextWordtHet;
+         sayItLoud(dezeTextWordtHet); // say something nice say it loud
+         selectThisImage(); // show a nice image
+    }
+    
+    
+    
+    function randomizer(range = 1){ 
+        // Math.random geeft een waarde  0 <= waarde < 1 (dus exclusief 1)
+        return Math.floor((Math.random() * range));
+    }
+    
+    
+    function makeThisSentence(){
+        let index = randomizer(arrayLength);
+        let index2 = randomizer(arrayLength);
+        let index3 = randomizer(arrayLength);
+        let woord1 = onderwerp[index];
+        let woord2 = werkwoord[index2];
+        let woord3 = restwoord[index3];
+        let outputString = woord1 + " " + woord2 + " " + woord3;
+        return outputString;
+    }
+    
+    function selectThisImage(){
+        let index = randomizer(arrayLength);
+        myImage.src = plaatjes[index]; 
+    }
+    
+    function sayItLoud (textString) {
+      let message = new SpeechSynthesisUtterance(textString);
+      let voices = window.speechSynthesis.getVoices();
+      message.voice = voices[1];
+      message.pitch = 1; // range between 0 and 2
+      message.rate = 1; // range between 0.1 (lowest) and 10 (highest) 
+      window.speechSynthesis.speak(message);
+    }
+    
+    
+    
+    const onderwerp = ["it", "Bram", "Harry Potter"];
+    const werkwoord = ["smells", "sees", "drinks"];
+    const restwoord = ["a magical potion", "4 bottles", "a powerdrill"];
+        
+    let plaatjes = ["550x493.jpg", "unknowqqn.png", "3325d90043e779964a97c6f668b51950.jpg", "Screenshot 2020-11-11 004033.png"]
+    let arrayLength = onderwerp.length;
